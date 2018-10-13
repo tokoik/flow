513194e7a1a6ba95c738d32b88b5c01e852642a9
Kohe Tokoi <tokoik@gmail.com>
2018-10-13 22:22:59 +0900

    Blob クラスを追加します。

---
+++ Blob.cpp
@@ -0,0 +1,66 @@
+﻿//
+// 粒子群オブジェクト
+//
+#include "Blob.h"
+
+// コンストラクタ
+Blob::Blob(const Particles &particles)
+  : count(static_cast<GLsizei>(particles.size()))
+{
+  // 頂点配列オブジェクトを作成する
+  glGenVertexArrays(1, &vao);
+  glBindVertexArray(vao);
+
+  // 頂点バッファオブジェクトを作成する
+  glGenBuffers(1, &vbo);
+  glBindBuffer(GL_ARRAY_BUFFER, vbo);
+  glBufferData(GL_ARRAY_BUFFER, count * sizeof(Particle), nullptr, GL_STATIC_DRAW);
+
+  // 結合されている頂点バッファオブジェクトを in 変数から参照できるようにする
+  glVertexAttribPointer(0, std::tuple_size<Vector>::value, GL_FLOAT, GL_FALSE,
+    sizeof(Particle), &static_cast<const Particle *>(0)->position);
+  glEnableVertexAttribArray(0);
+  glVertexAttribPointer(1, std::tuple_size<Vector>::value, GL_FLOAT, GL_FALSE,
+    sizeof(Particle), &static_cast<const Particle *>(0)->velocity);
+  glEnableVertexAttribArray(1);
+
+  // 頂点バッファオブジェクトにデータを格納する
+  initialize(particles);
+}
+
+// デストラクタ
+Blob::~Blob()
+{
+  // 頂点配列オブジェクトを削除する
+  glDeleteBuffers(1, &vao);
+
+  // 頂点バッファオブジェクトを削除する
+  glDeleteBuffers(1, &vbo);
+}
+
+// 初期化
+void Blob::initialize(const Particles &particles) const
+{
+  // 頂点バッファオブジェクトを結合する
+  glBindBuffer(GL_ARRAY_BUFFER, vbo);
+
+  //  頂点バッファオブジェクトにデータを格納する
+  Particle *p(static_cast<Particle *>(glMapBuffer(GL_ARRAY_BUFFER, GL_WRITE_ONLY)));
+  for (auto particle : particles)
+  {
+    p->position = particle.position;
+    p->velocity = particle.velocity;
+    ++p;
+  }
+  glUnmapBuffer(GL_ARRAY_BUFFER);
+}
+
+// 描画
+void Blob::draw() const
+{
+  // 描画する頂点配列オブジェクトを指定する
+  glBindVertexArray(vao);
+
+  // 点で描画する
+  glDrawArrays(GL_POINTS, 0, count);
+}
---
+++ Blob.h
@@ -0,0 +1,45 @@
+﻿#pragma once
+#include "Particle.h"
+
+// 標準ライブラリ
+#include <vector>
+
+//
+// 粒子群データ
+//
+using Particles = std::vector<Particle>;
+
+//
+// 粒子群オブジェクト
+//
+class Blob
+{
+  // 頂点配列オブジェクト名
+  GLuint vao;
+
+  // 頂点バッファオブジェクト名
+  GLuint vbo;
+
+  // 頂点の数
+  const GLsizei count;
+
+public:
+
+  // コンストラクタ
+  Blob(const Particles &particles);
+
+  // デストラクタ
+  virtual ~Blob();
+
+  // コピーコンストラクタによるコピー禁止
+  Blob(const Blob &blob) = delete;
+
+  // 代入によるコピー禁止
+  Blob &operator=(const Blob &blob) = delete;
+
+  // 初期化
+  void initialize(const Particles &particles) const;
+
+  // 描画
+  void draw() const;
+};
